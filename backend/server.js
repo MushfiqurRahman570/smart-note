2ee5d6eb0aaf049cc2bd5e32b4951a6b599c45da
MushfiqurRahman570 <your-email@example.com>
2025-05-13 16:49:26 +0900

    port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const path = require('path');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
